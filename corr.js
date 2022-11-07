7e54621a7917451290fd8eeae158dbbe56f28787
Gabriel <gabrielnascimentosantana.ba@gmail.com>
2022-11-07 20:38:06 -0300

    primeiro commit

---
+++ corr.js
@@ -0,0 +1,27 @@
+function enviar(){
+    var cor = String(escreva.value)
+
+    if(cor === "preto"){
+        document.body.style.background = "black"
+    }
+    if(cor ==="vermelho"){
+        document.body.style.background = "red"
+    }
+    if(cor ==="azul"){
+        document.body.style.background = "blue"
+    
+    }
+    if(cor ==="amarelo"){
+        document.body.style.background = "yellow"
+    }
+    if(cor ==="verde"){
+        document.body.style.background = "green"
+    }
+    if(cor ==="rosa"){
+        document.body.style.background = "pink"
+    }
+    if(cor ==="laranja"){
+        document.body.style.background = "orange"
+
+    }
+}
